02568a8bb04012940dec361cf0f5f82592358e05
Veronica Pardo <veronica.pardo@ucb.edu.bo>
2026-05-30 19:39:04 -0400

    arreglo de la coneccion a la BD de mi URI propia

--- database.py
+++ database.py
@@ -1,13 +1,13 @@
 from pymongo import MongoClient
-
+import certifi
 # Dejamos la URI armada de forma genérica para que no rebote tu contraseña real en el GitHub del grupo
-MONGO_URI = 'mongodb+srv://aaronherbas123321_db_user:A87a94a109a@cluster0.olqxkhn.mongodb.net/?appName=Cluster0'
+MONGO_URI = "mongodb+srv://veronica:base2026@cluster0.rewf6ma.mongodb.net/?appName=Cluster0"
 
 def dbConnection():
     try:
-        client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
+        client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000, tlsCAFile=certifi.where())
         db = client["dbb_products_app"]
         return db
     except Exception as e:
-        print("Error de conexión con la bdd:", e)
-        return None
+        print("Error de conexión con la bd:", e)
+        return Nonepy
