1541f0564e1f6d1bb55b3668a75f1d94a1dcaa19
Mauritzio1486 <diazsolizmauricio1@gmail.com>
2026-06-01 08:34:15 -0400

    Fix: Asegurar que el prcio numerico

--- Tienda-de-Ropa-main/product.py
+++ Tienda-de-Ropa-main/product.py
@@ -2,9 +2,11 @@ class Product:
     def __init__(self, name, description, price, stock, category, image):
         self.name = name
         self.description = description
-        # Convertimos a número decimal (float) y entero (int) al recibir los datos
+        
+      
         self.price = float(price) if price else 0.0
         self.stock = int(stock) if stock else 0
+        
         self.category = category
         self.image = image
 
@@ -12,8 +14,8 @@ class Product:
         return {
             "nombre": self.name,
             "descripcion": self.description,
-            "precio": self.price,  # Se guardará como número
-            "stock": self.stock,    # Se guardará como número
+            "precio": self.price,   # Ahora se guardará de color morado en Compass (Número)
+            "stock": self.stock,
             "categoria": self.category,
             "imagen": self.image
         }
